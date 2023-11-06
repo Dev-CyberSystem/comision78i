17b94b430ac2b0b86757471bc62a56ff3b076112
JavierRodriguez3 <javrodriguez2003@gmail.com>
2023-11-06 10:40:56 -0300

    Primer practico js Javier Rodriguez

---
+++ js/PrimerPractico.js
@@ -0,0 +1,34 @@
+function suma(){
+    let num1 = 1
+    let num2 = 2
+    let suma = num1 + num2
+    document.write("La suma es: " + suma) 
+    console.log(num2)
+}
+suma()
+
+let num3 = 29
+let num4 = 1
+function suma1(num3, num4){
+    let suma1 = num3 + num4
+    document.write("La suma es: " + suma1)
+}
+suma1(num3, num4)
+ 
+const pi = 3.41
+let nombre = 'Javier Rodriguez'
+let x = false
+
+console.log(pi)
+console.log(num3)
+console.log(num4)
+console.log(nombre)
+console.log(x)
+
+
+
+// Crear Variables con let y const
+// Variables con tipos de datos: Number, String, Boolean
+// Usar la consola, Usar script en html.
+// Tienen que tener un index.html y un archivo .js
+// Nombres de los archivos primerPractico.js
