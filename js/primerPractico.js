cad817a156b0dc95ebe95a7415ce513302ffc502
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-11-06 12:17:50 -0300

    Primer Practico, terminado

---
+++ js/primerPractico.js
@@ -0,0 +1,13 @@
+// Crear Variables con let y const
+// Variables con tipos de datos: Number, String, Boolean
+let numero = 6
+let apodo = "Juanito"
+let valor = true
+const dni = 44001122
+const nombre = "Juan"
+const fijo = false
+// Usar la consola, Usar script en html.
+console.log("Numero: " + numero)
+console.log("Nombre: " + nombre)
+console.log("Fijo: " + fijo)
+alert(apodo + " - " + dni + " - " + valor)
