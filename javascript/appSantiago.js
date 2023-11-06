026892c726ff30b6cd36b298d2d34ab08473dca7
Santiago Soraire <eneasvallejo04@gmail.com>
2023-11-06 13:44:15 -0300

    Commit práctico de JavaScript

---
+++ javascript/appSantiago.js
@@ -0,0 +1,18 @@
+function DivisionDeNumeros() {
+    let numero1 = 10
+    let numero2 = 5
+    let resultado = numero1 / numero2
+    console.log("El resultado de la division es " + resultado)
+    document.write("Se divió " + numero1 + " entre " + numero2)
+    document.write(" Puedes ver el resultado de la división en la consola!")
+}
+DivisionDeNumeros()
+
+console.log(10 === "10")
+console.log(5 > 10)
+console.log(5 < 10)
+console.log(10 >= 20)
+console.log(10 == "10")
+console.log("Santiago" === "Santiag0")
+
+alert("Bienvenido!\nEsta es mi prueba de Javascript")
