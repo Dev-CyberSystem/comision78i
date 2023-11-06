a9554318baf7f2197a22650fe4b85f89b13eb813
JavierRodriguez3 <javrodriguez2003@gmail.com>
2023-11-06 10:35:25 -0300

    Primeras pruebas js Javier

---
+++ js/primeraJs.js
@@ -0,0 +1,16 @@
+function suma(){
+    let num1 = 1
+    let num2 = 2
+    let suma = num1 + num2
+    document.write("La suma es: " + suma) 
+}
+suma()
+
+let num3 = 29
+let num4 = 1
+function suma1(num3, num4){
+    let suma1 = num3 + num4
+    document.write("La suma es: " + suma1)
+}
+suma1(num3, num4)
+ 
