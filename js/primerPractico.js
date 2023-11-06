4a84f41d9cc1e785d865aa79547bcc8d169fc438
Facundo Arebalo <facuar1589@gmail.com>
2023-11-06 17:34:51 -0300

    practico javascript

--- js/primerPractico.js
+++ js/primerPractico.js
@@ -5,7 +5,18 @@ function suma(){
     document.write("La suma es: " + suma) 
     console.log(suma)
 }
+
 suma()
+
+function resta(){
+    let numero3 = 25
+    let numero4 = 35
+    let resta = numero3 - numero4
+    document.write("La resta es: " + resta) 
+    console.log(resta)
+}
+
+resta()
 
 
  
@@ -15,3 +26,16 @@ let nombre = 'Facundo'
 console.log(nombre)
 let casado = false
 console.log(casado)
+
+let edad = 24
+console.log(edad)
+
+let humano = true
+console.log('humano', humano)
+
+const numFav = 10
+document.write('numeroFav es 10')
+console.log(numFav)
+
+
+
