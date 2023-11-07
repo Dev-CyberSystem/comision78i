a07568d86ddde939ef5cb1992049241661aa7610
lourdesreyna <reynalourdes91@gmail.com>
2023-11-07 20:06:00 -0300

    operadores de comparación

---
+++ javascript/1erPráctico.js
@@ -0,0 +1,17 @@
+//operadores de comparación (booleano)
+// 20 es mayor o igual a 10
+20 >= 10 // true
+// 2 es menor que 3
+2 < 3 // true
+// 7 es mayor que 10
+7 > 10 // false
+// 10 es igual a 10 
+10 === 10 // true (mismo datos en valor y tipo de datos)
+// 5 es igual a cinco (en valor solamente)
+5 == cinco // false (busca el numero y no la letra)
+5 == 5 // true (mismo valor)
+6 !== 10 // true
+8 !== '8' // true (mismo valor pero distinto tipo de dato)
+
+
+
