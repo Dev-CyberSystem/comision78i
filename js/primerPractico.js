d7ac1fd80f1710058cea08da6fc013ebc15bc12b
Pablo Moyano <pablomoyano08@gmail.com>
2023-11-07 16:22:21 -0300

    Subiendo Primer Practico

---
+++ js/primerPractico.js
@@ -0,0 +1,16 @@
+// Crear Variables con let y const
+// Variables con tipos de datos: Number, String, Boolean
+let numero = 10; // Variable numérica
+let mensaje = "Bienvenido"; 
+const saludo = "Hola, mundo"; // Variable de texto
+const esVerdadero = true; // Variable booleana
+// Usar la consola, Usar script en html.
+console.log("Número:", numero);
+console.log("Saludo:", saludo);
+console.log("¿Es verdadero?", esVerdadero);
+
+//operaciones con las variables
+let resultado = numero * 2;
+console.log("Resultado:", resultado);
+
+alert(mensaje);
