6385bd54d421331c6a83237625b6644c11083ca2
marianoruiza <marianoruiz145@gmail.com>
2023-11-07 00:03:23 -0300

    Tarea Nro 1 js Mariano Ruiz

---
+++ tarea Nro 1/primerPractico.js
@@ -0,0 +1,24 @@
+function muestra() {
+    let num = 10
+    const num2 = 20
+    let char = 'mariano'
+    let boolean = true
+    let suma = num + num2    
+
+    console.log( "Numero: " + num) 
+    console.log( "Constante: " + num2 )
+    console.log( "Suma: " + suma )
+    console.log("Caracter: " + char)
+    console.log( "Booleano: " + boolean)
+     
+}
+muestra()
+ 
+
+
+// Crear Variables con let y const
+// Variables con tipos de datos: Number, String, Boolean
+// Usar la consola, Usar script en html.
+// Tienen que tener un index.html y un archivo .js
+// Nombres de los archivos primerPractico.js
+
