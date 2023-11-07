705a1934cbd437ed6c850f5a4dec6adfbc0e84da
unknown <maria.g.monteros@gmail.com>
2023-11-07 17:33:50 -0300

    se crea carpeta js

---
+++ javascript/gabriela.js
@@ -0,0 +1,23 @@
+
+
+// function sumarNumeros() {
+//     let num1 = 50  
+//     let num2 = 50
+    
+//     let suma= num1 + num2
+//     document.write ("la suma es : " + suma)
+    
+// }
+// sumarNumeros()
+
+
+    function sumarNumeros(){
+        let num1 =  10
+        let num2 = 1
+
+        let suma= num1 + num2
+        document.write ("la suma es :" + suma)
+
+    }
+
+    restarNumeros()
