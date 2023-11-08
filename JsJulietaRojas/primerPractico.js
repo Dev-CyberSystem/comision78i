2295569679daf3d3339fe453bf4586d24b4d8d55
JulietaRojas <julietar39@gmail.com>
2023-11-08 03:21:36 -0300

    Agregando Carpeta a la rama dev

---
+++ JsJulietaRojas/primerPractico.js
@@ -0,0 +1,20 @@
+function Numeros() {
+    let numero1 = 20
+    let numero2 = 7
+    let suma = numero1 + numero2
+    document.write("La suma es: " + suma)
+}
+Numeros()
+
+//String
+    let mensaje= "Hola,soydelacomision78i";
+    console.log(mensaje)
+
+
+function verificarEdad(edad, esMayor) {
+    console.log("edad: " + edad)
+    console.log("es mayor: " + esMayor)
+
+}
+verificarEdad(27,true);
+   
