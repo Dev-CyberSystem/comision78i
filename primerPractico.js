7f7c6bf205724904e4ec837ed13e2239f5ce3dae
Enzo Castro <enzocastro110@gmail.com>
2023-11-08 08:10:38 -0300

    practica

--- primerPractico.js
+++ primerPractico.js
@@ -1,10 +1,9 @@
 function practicaJs () {
     let number1 = 25
     let number2 = 2023
-    let name1 = Enzo
-    let lastName1 = Castro
-    const secondName = Matias
-    let resta = number1 - number2
-    document.write("Enzo practicando" + resta)
+    let name1 = "Enzo"
+    let lastName1 = "Castro"
+    const secondName = "Matias"
+    let resta = number2 - number1
+    document.write("Enzo practicando " + resta)
 } practicaJs()
-
