dfc528abeeb7b8f309709acf0b42850dd55a310a
JulietaRojas <julietar39@gmail.com>
2023-11-08 02:50:55 -0300

    primerPractJs

---
+++ Js JulietaRojas/primerPractico.js
@@ -0,0 +1,21 @@
+function Numeros() {
+    let numero1 = 20
+    let numero2 = 7
+    let suma = numero1 + numero2
+    document.write("La suma es: " + suma)
+}
+Numeros()
+
+function soloString() {
+    let mensaje: "Hola soy de la comision 78i"
+    console.log(mensaje)
+}
+soloString()
+
+function verificarEdad(edad, esMayor) {
+    console.log("edad: " + edad)
+    console.log("es mayor: " + esMayor)
+
+}
+verificarEdad(27,true):
+   
