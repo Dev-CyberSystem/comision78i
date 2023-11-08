d74f71272944e4903b388a542cddb66599db5d34
unknown <maria.g.monteros@gmail.com>
2023-11-08 07:03:08 -0300

    prrimer practica

--- javascript/gabriela.js
+++ javascript/gabriela.js
@@ -1,23 +1,17 @@
 
 
-// function sumarNumeros() {
-//     let num1 = 50  
-//     let num2 = 50
+function sumarNumeros() {
+    let num1 = 50  
+    let num2 = 50
     
-//     let suma= num1 + num2
-//     document.write ("la suma es : " + suma)
+    let suma= num1 + num2
+    document.write ("la suma es : " + suma)
+    console.log ("Valor de num2")
     
-// }
-// sumarNumeros()
+    
+}
+sumarNumeros()
+
+10==10
 
 
-    function sumarNumeros(){
-        let num1 =  10
-        let num2 = 1
-
-        let suma= num1 + num2
-        document.write ("la suma es :" + suma)
-
-    }
-
-    restarNumeros()
