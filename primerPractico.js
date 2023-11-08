65465b1c648f37133ba69fd801d19486effba1c8
Enzo Castro <enzocastro110@gmail.com>
2023-11-08 00:13:04 -0300

    Práctica Enzo

---
+++ primerPractico.js
@@ -0,0 +1,10 @@
+function practicaJs () {
+    let number1 = 25
+    let number2 = 2023
+    let name1 = Enzo
+    let lastName1 = Castro
+    const secondName = Matias
+    let resta = number1 - number2
+    document.write("Enzo practicando" + resta)
+} practicaJs()
+
