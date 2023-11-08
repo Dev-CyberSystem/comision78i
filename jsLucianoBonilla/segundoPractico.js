5e77d109eca9a9692fe9a77e7e291025cf23bbd2
Luciano <lucianobonilla27@gmail.com>
2023-11-08 12:06:36 -0300

    segundo practico resuelto

---
+++ jsLucianoBonilla/segundoPractico.js
@@ -0,0 +1,130 @@
+// Crea dos variables con valores numéricos diferentes y compáralas usando el operador de igualdad (==). Muestra el resultado en la consola.
+// let a=1;
+// let b=10;
+// console.log(a == b)
+
+// Crea dos variables con valores numéricos diferentes y compáralas usando el operador de desigualdad (!=). Muestra el resultado en la consola.
+// let a=1;
+// let b=10;
+// console.log(a != b)
+
+// Crea una variable con un valor numérico y aumenta su valor en 5 utilizando el operador de incremento (++). Muestra el resultado en la consola.
+// let a = 0
+// for (let index = 0; index < 5; index++) {
+//     a++
+// }
+// console.log(a);
+
+// Crea una variable con un valor numérico y disminuye su valor en 3 utilizando el operador de decremento (--). Muestra el resultado en la consola.
+// let a = 5
+// for (let index = 0; index < 3; index++) {
+//     a--
+// }
+// console.log(a);
+
+// Crea una variable que almacene una cadena de texto y utiliza el operador de concatenación (+) para agregar más texto al final de la cadena. Muestra el resultado en la consola.
+// let texto = "hola"
+// let nombre = "Juan"
+// console.log(texto + " " + nombre)
+
+// Crea dos variables con valores booleanos diferentes y utiliza el operador lógico AND (&&) para compararlas. Muestra el resultado en la consola.
+// let a = true
+// let b = false
+// console.log(a && b)
+
+// Crea dos variables con valores booleanos diferentes y utiliza el operador lógico OR (||) para compararlas. Muestra el resultado en la consola.
+// let a = true
+// let b = false
+// console.log(a || b)
+
+// Crea una variable que almacene un valor numérico y utiliza el operador de comparación mayor que (>) para compararlo con otro valor numérico. Muestra el resultado en la consola.
+// let a = 5
+// let b = 10
+// console.log(a > b)
+
+// Crea una variable que almacene un valor numérico y utiliza el operador de comparación menor o igual que (<=) para compararlo con otro valor numérico. Muestra el resultado en la consola.
+// let a = 5
+// let b = 10
+// console.log(a <= b)
+
+// Crea una Funcion que reciba dos parametros y los sume, luego muestra el resultado en la consola.
+// function suma(a,b) {
+//     console.log(a+b)
+// }
+// suma(10,10)
+
+// Crea una Funcion que reciba dos parametros y los reste, luego muestra el resultado en la consola.
+// function resta(a,b) {
+//     console.log(a-b)
+// }
+// resta(10,10)
+
+// Crea una funcion donde se declare una variable con el valor 5 y se le sume 10, luego muestra el resultado en la consola.
+// function suma10() {
+//     let a = 5
+//     a += 10
+//     console.log(a)
+// }
+// suma10()
+
+// Crea una funcion donde se declaren dos variables con valores en string y se concatenen, luego muestra el resultado en la consola.
+// function concatenar(){
+//     let a = "primero"
+//     let b = "segundo"
+//     let concatenado = a + "," + b
+//     console.log(concatenado)
+// }
+// concatenar()
+
+// Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son iguales, luego muestra el resultado en la consola.
+// function iguales() {
+//     let a = true
+//     let b = false
+//     console.log(a==b)
+// }
+// iguales()
+
+// Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son diferentes, luego muestra el resultado en la consola.
+// function diferentes() {
+//     let a = true
+//     let b = false
+//     console.log(a!=b)
+// }
+// diferentes()
+
+// Crea una funcion donde se declaren dos variables con valores numericos y se compare si el primero es mayor que el segundo, luego muestra el resultado en la consola.
+// function mayor() {
+//     let primero = 5
+//     let segundo = 2
+//     console.log(primero > segundo)
+// }
+// mayor()
+
+// Mediante un prompt pide al usuario que ingrese un numero y luego muestra el resultado en la consola.
+// const a = prompt("ingrese un numero")
+// console.log("numero ingresado = " + a)
+
+// Mediante un prompt pide al usuario que ingrese su nombre y luego muestra el resultado en la consola.
+// const nombre = prompt("ingrese un nombre")
+// console.log("Nombre ingresado: " + nombre)
+
+// Mediante un prompt pide al usuario que ingrese dos numeros, estos se deben almacenar en dos variables diferentes, luego haz una suma de ambos y que el resultado se guarde en otra variable, muestra el resultado de la suma en el documento.
+// const a = prompt("ingrese el primero numero")
+// const b = prompt("ingrese el segundo numero")
+// suma = parseInt(a) + parseInt(b)
+// document.write(suma)
+
+//  Escribe un programa que solicite al usuario su año de nacimiento y calcule su edad actual y muestre si es o no mayor de edad. Luego, muestra la edad en una cadena de texto.
+// const nacimiento = prompt("Ingrese su año de nacimiento")
+// let edad = 2023 - nacimiento
+// if (edad >= 18) {
+//     console.log(`Usted tiene ${edad} años, es mayor de edad`)
+// }else{
+//     console.log(`Usted tiene ${edad} años, es menor de edad`)
+
+// }
+
+// Crea un programa que cuente el número de palabras en una frase proporcionada por el usuario. Una palabra se define como cualquier secuencia de caracteres separada por espacios en blanco.
+// const frase = prompt("Ingrese una frase")
+// let separar = frase.split(" ");
+// console.log("Su frase tiene " + separar.length + " palabras")
