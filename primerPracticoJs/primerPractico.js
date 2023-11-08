fbee2fea48a5956accd7428c394fd0cebd060571
Monica Ruiz <rumoni85@gmail.com>
2023-11-07 22:12:25 -0300

    primer practico de js

---
+++ primerPracticoJs/primerPractico.js
@@ -0,0 +1,16 @@
+let num1 = 9
+let num2 = 4
+let saludo = "Hola"
+let nom = "Monica"
+const x= true
+const y = false
+
+console.log(saludo +" " + nom)
+
+if(num1 > num2){
+    console.log(x)
+}else{
+    console.log(y)
+}
+
+console.log(num1 + num2)
