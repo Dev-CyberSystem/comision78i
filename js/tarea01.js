4da32d8e3a64ff0e1191b470491a0692598f3c83
keyraAzure <klimac4@miumg.edu.gt>
2023-11-07 23:02:23 -0600

    cambios tarea 1

---
+++ js/tarea01.js
@@ -0,0 +1,37 @@
+//Variables en JavaScript
+//1-Tres tipos: var, let y const
+
+//var = variable global en todo el doc
+var numero1 = 32;
+var numero2 = 40;
+
+console.log(numero1);
+console.log(numero2);
+
+//let = variable nivel de bloque
+let x = 22;
+let y = 44;
+
+console.log(x);
+console.log(y);
+
+//const = variable de bloque que es constante (no cambia)
+const pi = 3.1416;
+console.log("El valor de PI es ", pi);
+
+//2- Variables con tipos de datos
+let nombre = "Keyra"
+let edad = 22;
+let saludo = "Bienvenida";
+let valor = true;
+
+console.log("Buenos dias ", nombre, " ", saludo, "tu edad es: ", edad);
+
+//Operaciones con Variables
+let num1 = 22;
+let num2 = 44;
+let num3 = 56;
+let resultado = (num1 + num2 + num3);
+console.log("El resultado de la operacion Suma es: ", resultado);
+
+alert(saludo)
