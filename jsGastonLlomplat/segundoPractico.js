0a68853056de41ecc2bf0d5c02af5e22d9b58073
GastonGLL <gastongutierrezllomplat@gmail.com>
2023-11-09 16:38:25 -0300

    Segunda parte de la tarea

--- jsGastonLlomplat/segundoPractico.js
+++ jsGastonLlomplat/segundoPractico.js
@@ -1,9 +1,9 @@
 //Realiza una operación matemática simple y almacena el resultado en una variable. Luego, muestra el resultado en la consola.
 
-let num1 = parseInt(prompt("Ingresa el primer número a sumar"))
-let num2 = parseInt(prompt("Ingresa el segundo número a sumar"))
-let resultado = (num1 + num2)
-console.log(`El resultado de la suma de los dos números es ${resultado}`)
+let num1 = 50
+let num2 = 37
+let resultado = num1 + num2
+console.log(`El resultado de la suma de los dos números (${num1} y ${num2}) es ${resultado}`)
 
 // Crea dos variables con valores numéricos diferentes y compáralas usando el operador de igualdad (==). Muestra el resultado en la consola.
 
@@ -37,10 +37,34 @@ diferencia()
 
 // Crea una variable con un valor numérico y disminuye su valor en 3 utilizando el operador de decremento (--). Muestra el resultado en la consola.
 // Crea una variable que almacene una cadena de texto y utiliza el operador de concatenación (+) para agregar más texto al final de la cadena. Muestra el resultado en la consola.
+
+let oracion = "Juegue deca querido. "
+let palabraFinal = oracion + "Celeste y Blanco todo Tucumán."
+console.log(palabraFinal)
+
 // Crea dos variables con valores booleanos diferentes y utiliza el operador lógico AND (&&) para compararlas. Muestra el resultado en la consola.
+
+let bool_uno = true
+let bool_dos = false
+let compBool = (bool_uno && bool_dos)
+console.log(compBool)
+
 // Crea dos variables con valores booleanos diferentes y utiliza el operador lógico OR (||) para compararlas. Muestra el resultado en la consola.
+
+let compBool_dos = (bool_uno || bool_dos)
+console.log(compBool_dos)
+
 // Crea una variable que almacene un valor numérico y utiliza el operador de comparación mayor que (>) para compararlo con otro valor numérico. Muestra el resultado en la consola.
+
+let variable1 = 10
+let variable2 = 20
+let menorNoma = (variable1 > variable2)
+console.log(menorNoma)
+
 // Crea una variable que almacene un valor numérico y utiliza el operador de comparación menor o igual que (<=) para compararlo con otro valor numérico. Muestra el resultado en la consola.
+
+let menorIgual = (variable1 <= variable2)
+console.log(menorIgual)
 
 // Crea una Funcion que reciba dos parametros y los sume, luego muestra el resultado en la consola.
 
@@ -68,8 +92,8 @@ sumandoDosNumeros()
 // Crea una funcion donde se declaren dos variables con valores en string y se concatenen, luego muestra el resultado en la consola.
 
 function concatenacion() {
-    let equipo = prompt("¿De qué equipo sos?")
-    let jerga = prompt("¿Cómo le dicen a tu equipo?")
+    let equipo = "Atlético Tucumán"
+    let jerga = "El Gigante del Norte"
     let union = equipo + " " + jerga
     console.log(union)
 }
@@ -77,7 +101,20 @@ concatenacion()
 
 // Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son iguales, luego muestra el resultado en la consola.
 // Crea una funcion donde se declaren dos variables con valores booleanos y se compare si son diferentes, luego muestra el resultado en la consola.
+
 // Crea una funcion donde se declaren dos variables con valores numericos y se compare si el primero es mayor que el segundo, luego muestra el resultado en la consola.
+
+function mayorMenor() {
+    let numero_uno = 73
+    let numero_dos = 37
+    if (numero_uno > numero_dos) {
+        return console.log(`El primer número ingresado (${numero_uno}) es mayor que el segundo (${numero_dos})`)
+    }
+    else {
+        return console.log(`El primer número ingresado (${numero_uno}) es menor que el segundo (${numero_dos})`)
+    }
+}
+mayorMenor()
 
 // Mediante un prompt pide al usuario que ingrese un numero y luego muestra el resultado en la consola.
 
@@ -90,10 +127,25 @@ let nombre = prompt("Ingrese su nombre")
 console.log(`Su nombre es ${nombre}`)
 
 // Mediante un prompt pide al usuario que ingrese dos numeros, estos se deben almacenar en dos variables diferentes, luego haz una suma de ambos y que el resultado se guarde en otra variable, muestra el resultado de la suma en el documento.
+
+let num3 = parseInt(prompt("Ingrese el primer número a sumar (se mostrará el resultado en el documento)"))
+let num4 = parseInt(prompt("Ingrese el segundo número a sumar (se mostrará el resultado en el documento)"))
+let resultado5 = (num3 + num4)
+document.write(`El resultado de la suma de los dos números es ${resultado5}`)
+
 //  Escribe un programa que solicite al usuario su año de nacimiento y calcule su edad actual y muestre si es o no mayor de edad. Luego, muestra la edad en una cadena de texto.
+
+function calculoEdad() {
+    let edad = parseInt(prompt("Ingrese su año de nacimiento"))
+    let edadActual = (2023-edad)
+    if (edadActual > 18) {
+        return console.log(`Usted es mayor de edad. Tiene ${edadActual} años`)
+    }
+    else {
+        return console.log(`Usted es menor de edad. Tiene ${edadActual} años`)
+    }
+}
+calculoEdad()
+
 // Crea un programa que cuente el número de palabras en una frase proporcionada por el usuario. Una palabra se define como cualquier secuencia de caracteres separada por espacios en blanco.
 
-
-
-
-//Realiza una operación matemática simple y almacena el resultado en una variable. Luego, muestra el resultado en la consola.
