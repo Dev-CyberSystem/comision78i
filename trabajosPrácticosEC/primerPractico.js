6880f570802173a0f2c37a7dfd6ae7fd158c8a7d
Enzo Castro <enzocastro110@gmail.com>
2023-11-10 14:31:16 -0300

    Actualizo mi branch

---
+++ trabajosPrácticosEC/primerPractico.js
@@ -0,0 +1,9 @@
+function practicaJs () {
+    let number1 = 25
+    let number2 = 2023
+    let name1 = "Enzo"
+    let lastName1 = "Castro"
+    const secondName = "Matias"
+    let resta = number2 - number1
+    document.write("Enzo practicando " + resta)
+} practicaJs()
