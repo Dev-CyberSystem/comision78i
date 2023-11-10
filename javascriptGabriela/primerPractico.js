202b765b25d78013c9df66fd01c71448f689d027
unknown <maria.g.monteros@gmail.com>
2023-11-10 09:15:37 -0300

    primer practico

---
+++ javascriptGabriela/primerPractico.js
@@ -0,0 +1,40 @@
+// 10 >=9 //10 mayor o igual que nueve
+// 0===0 // 0 es igual a cero
+
+// 5 !== 5 //false
+// 10 !== 8 // true
+
+// 6 <=2 //false
+// 6 >=10 // true
+// 9 < 2 // false
+// 2 < 1 //true
+
+// //comparacion de cadenas de texto
+
+// "Gabriela" === "Juan Martin" // false
+// "Estudio"  === 'Estudio'     // true
+// "Javascript" !== "Java"      // true
+// "Javascript" === "Java"      // false
+
+// //Operadores Logicos
+
+// true && true  // true
+// true && false // false
+// false && false // false
+
+// true || true // true
+// true || false // true
+// false || false // false
+
+// !true // false
+// !false // true
+
+// //variables
+
+// let number = 4 
+//  number + 2 // resultado 4 + 2 =6
+
+// let mensaje = "Hola Mundo"
+
+
+console.log (nombre)
