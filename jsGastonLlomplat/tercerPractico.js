ca442e682fd2e264df24997acef075ade0a56788
GastonGLL <gastongutierrezllomplat@gmail.com>
2023-11-14 20:56:59 -0300

    Tarea 3 Clase

---
+++ jsGastonLlomplat/tercerPractico.js
@@ -0,0 +1,221 @@
+// Crear una función que tome un número como parámetro y devuelva "Positivo" si es mayor a cero, "Negativo" si es menor a cero o "Cero" si es igual a cero.
+
+const numPosNegCero = (num) => {
+    if (num > 0) {
+        return console.log("Positivo")
+    }
+    else if (num < 0) {
+        return console.log("Negativo")
+    }
+    else {
+        return console.log("Cero")
+    }
+}
+numPosNegCero(0)
+
+// Crear una función que tome un número como parámetro y devuelva "Par" si es divisible por 2 o "Impar" si no lo es.
+
+const numParImpar = (numero) => {
+    if (numero % 2 == 0 ) {
+        return console.log("Par")
+    } else {
+        console.log("Impar")
+    }
+}
+numParImpar(73)
+
+// Crear un programa que pida al usuario un número y luego muestre si es par o impar.
+
+const numParImparUsuario = () => {
+    let numParImpar = prompt("Ingrese un número")
+    if (numParImpar % 2 == 0 && numParImpar != 0 ) {
+        return console.log("Par")
+    } else if (numParImpar % 2 != 0) {
+        return console.log("Impar")
+    } else {
+        console.log("Cero")
+    }
+}
+//numParImparUsuario()
+
+// Crear un programa que pida al usuario un número y luego muestre si es positivo, negativo o cero.
+
+const numPosNegCeroUsuario = () => {
+    let numPosNegUsuario = prompt("Ingrese un número")
+    if (numPosNegUsuario > 0) {
+        return console.log("Positivo")
+    }
+    else if (numPosNegUsuario < 0) {
+        return console.log("Negativo")
+    }
+    else {
+        return console.log("Cero")
+    }
+}
+//numPosNegCeroUsuario()
+
+// Crear un programa que pida al usuario dos números y luego muestre cuál es el mayor de los dos.
+
+const numMayorMenor = () => {
+    let numero1 = prompt("Ingrese el primer número")
+    let numero2 = prompt("Ingrese el segundo número")
+    if (numero1 > numero2) {
+        return console.log(`Es mayor ${numero1}`)
+    } else if (numero2 > numero1) {
+        return console.log(`Es mayor ${numero2}`)
+    } else {
+        console.log("Los números son iguales")
+    }
+}
+//numMayorMenor()
+
+// Crear un programa que pida al usuario una contraseña y la valide según ciertas condiciones, como la longitud y la presencia de caracteres especiales.
+
+// Crear un programa que pida al usuario una calificación y luego muestre si es aprobatoria o no (nota mínima para aprobar es 70).
+
+const notaAprobatoria = () => {
+    let nota = prompt("Ingrese su calificación (0 a 100)")
+    if (nota >= 70 && nota <= 100) {
+        return console.log("Usted ha aprobado")
+    } else if (nota < 70 && nota >= 0) {
+        return console.log("Usted ha desaprobado")
+    } else {
+        return console.log("Nota no válida")
+    }
+}
+//notaAprobatoria()
+
+// Crear una función que tome una cadena como parámetro y devuelva "Es un palíndromo" si la cadena es igual al revés, de lo contrario, devolver "No es un palíndromo".
+
+const palindromo = (cadena) => {
+    // split() Devuelve un nuevo arreglo - reverse() Invierte el arreglo - join() Une el arreglo en una cadena
+    cadenaReversa = cadena.split("").reverse().join("")
+    if (cadenaReversa == cadena) {
+        return console.log("Es un palíndromo")
+    } else {
+        return console.log("No es un palíndromo")
+    }
+}
+//palindromo("reconocer") 
+
+// Crear un programa que pida al usuario una letra y luego muestre si es una vocal o una consonante.
+
+const letraVocalConsonante = () => {
+    let letra = prompt("Ingrese una letra")
+    letra = letra.toUpperCase()
+    if (letra.length == 1) {
+         if (letra == "A" || letra == "E" || letra == "I" || letra == "O" || letra == "U") {
+        return console.log("Vocal")
+        } else {
+        return console.log("Consonante")
+        }
+    } else {
+        return console.log("No es una letra")
+    }
+}
+// letraVocalConsonante()
+
+// Crear un programa que pida al usuario un número del 1 al 7 y luego muestre el día de la semana correspondiente.
+
+const diaSemana = () => {
+    let dia = prompt("Ingrese un número del 1 al 7")
+    if (dia < 1 || dia > 7) {
+        return console.log("No cumple con la condición")
+    } else if (dia == 1) {
+        return console.log("Lunes")
+    } else if (dia == 2) {
+        return console.log("Martes")
+    } else if (dia == 3) {
+        return console.log("Miércoles")
+    } else if (dia == 4) {
+        return console.log("Jueves")
+    } else if (dia == 5) {
+        return console.log("Viernes")
+    } else if (dia == 6) {
+        return console.log("Sábado")
+    } else if (dia == 7) {
+        return console.log("Domingo")
+    }
+}
+// diaSemana()
+
+// Crear un programa que pida al usuario un número y luego muestre si es primo o no.
+
+const numeroPrimo = () => {
+    let n = prompt("Ingrese un número")
+    if ((n % n == 0 && n % 1 == 0 && n % 2 != 0) || (n ==2) ) {
+        return console.log("Es un número primo")
+    } else {
+        return console.log("No es un número primo")
+    }
+}
+//numeroPrimo()
+
+// Crear un programa que pida al usuario su edad y luego muestre si es mayor o menor de edad (18 años).
+
+const edadUsuario = () => {
+    let edad = prompt("Ingrese su edad")
+    if (edad >= 18) {
+        return console.log("Usted es mayor de edad")
+    } else if (edad < 18 && edad >= 0) {
+        return console.log("Usted es menor de edad")
+    } else {
+        return console.log("Edad no válida")
+    }
+}
+//edadUsuario()
+
+// Crear un programa que pida al usuario dos números y luego muestre si su suma es mayor, menor o igual a 100.
+
+const sumaMayMenCien = () => {
+    let num_uno = parseInt(prompt("Ingrese el primer número"))
+    let num_dos = parseInt(prompt("Ingrese el segundo número"))
+    if ((num_uno + num_dos) > 100) {
+        return console.log("La suma de los dos números es mayor a 100")
+    } else if ((num_uno + num_dos) < 100) {
+        return console.log("La suma de los dos números es menor a 100")
+    } else {
+        return console.log("La suma de los dos números es 100")
+    }
+}
+//sumaMayMenCien()
+
+// Crear un programa que pida al usuario tres números y luego muestre cuál es el mayor de los tres.
+
+const mayorTresNumeros = () => {
+    let numero_uno = parseInt(prompt("Ingrese el primer número"))
+    let numero_dos = parseInt(prompt("Ingrese el segundo número"))
+    let numero_tres = parseInt(prompt("Ingrese el tercer número"))
+    if (numero_uno > numero_dos && numero_uno > numero_tres) {
+        return console.log(`El número mayor es ${numero_uno}`)
+    } else if (numero_dos > numero_uno && numero_dos > numero_tres) {
+        return console.log(`El número mayor es ${numero_dos}`)
+    } else {
+        return console.log(`El número mayor es ${numero_tres}`)
+    }
+}
+//mayorTresNumeros()
+
+// Crear un programa que pida al usuario una letra y luego muestre si es una letra mayúscula o minúscula.
+
+const letraMayusMinus = () => {
+    let letra_ = prompt("Ingrese una letra")
+    if (letra_.length == 1) {
+        if (letra_ == letra_.toUpperCase()) {
+            return console.log("La letra es mayúscula")
+        } else {
+            return console.log("La letra es minúscula")
+        }
+    } else if (letra_.length > 1) {
+        return console.log("Ingresó más de una letra")
+    }
+}
+//letraMayusMinus()
+
+// Crear una función que tome un número como parámetro y devuelva "Fizz" si es divisible por 3, "Buzz" si es divisible por 5 o "FizzBuzz" si es divisible por ambos. En cualquier otro caso, devolver el número.
+
+// Crear un programa que pida al usuario un número y luego muestre si es un número perfecto o no. Un número perfecto es aquel que es igual a la suma de sus divisores propios.
+
+// Crear un programa que pida al usuario un número y luego muestre su descomposición en factores primos.
+
+// Crear un programa que pida al usuario una fecha (día, mes y año) y luego muestre si es válida o no. Una fecha es válida si cumple con ciertas condiciones, como la cantidad de días en el mes y si es un año bisiesto o no.
