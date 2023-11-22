e28c2e54f3cfab57a3965849ceb601b1567348a1
keyraAzure <klimac4@miumg.edu.gt>
2023-11-21 22:56:21 -0600

    tarea 04 pendiente

--- TareasJavascript/tarea03.js
+++ TareasJavascript/tarea03.js
@@ -35,3 +35,73 @@ function calculoProgram(){
         console.log("Impar");
     }
 }
+calculoProgram();
+
+// 4- Crear un programa que pida al usuario un número y luego muestre si es positivo, negativo o cero.
+function calculaSigno(){
+    let numeroIngresado = prompt("Ingresa un numero");
+    let numero = parseInt(numeroIngresado);
+
+    if(numero>0){
+        console.log("Positivo");
+    }else if(numero<0){
+        console.log("Negativo");
+    }else{
+        console.log("Es cero");
+    }
+}
+calculaSigno();
+
+//5-Crear un programa que pida al usuario dos números y luego muestre cuál es el mayor de los dos.
+function pideMayor(){
+    let num1 = prompt("Ingresa un numero");
+    let numero1 = parseInt(num1);
+
+    let num2 = prompt("Ingresa un numero");
+    let numero2 = parseInt(num2);
+
+    if(numero1>numero2){
+        console.log("Num1 es mayor que Num2");
+    }else if(numero2>numero1){
+        console.log("Num2 es mayor que Num1");
+    }else{
+        console.log("Los numeros son iguales");
+    }
+}
+pideMayor();
+
+/*6- Crear un programa que pida al usuario una contraseña 
+y valide según ciertas condiciones, como la longitud y la presencia de caracteres especiales.*/
+function contrasenia(){
+    let password = prompt('Ingresa una contraseña: Debe contener número, mayúscula y minúscula, debe ser de al menos 8 caracteres').trim();
+    if(password.length<8){
+        console.log("Contraseña muy corta")
+    }else if(!/[a-z]/.test(password)){
+        console.log('La contraseña debe tener al menos una minúscula');
+    }else if(!/[A-Z]/.test(password)){
+        console.log('La contraseña debe tener al menos una mayúscula');
+    }else if(!/\d/.test(password)){
+        console.log('La contraseña debe tener al menos un dígito');
+    }else{
+        console.log('contraseña creada con exito')
+    }      
+}
+contrasenia();
+
+/**7-Crear un programa que pida al usuario una calificación y luego muestre si es aprobatoria o no (nota mínima para aprobar es 70)*/
+function calificacion(){
+    let notaCC = prompt("Ingrese su nota");
+    let nota = parseInt(notaCC);
+
+    if(nota >= 70){
+        console.log("Curso aprobado");
+    }else{
+        console.log("Curso no aprobado");
+    }
+}
+calificacion();
+
+
+
+
+
--- TareasJavascript/tarea04.js
+++ TareasJavascript/tarea04.js
@@ -1,1 +1,93 @@
 //Tarea 04 - 15 noviembre
+
+// 1) Crea un programa que reciba un número del 1 al 12 y muestre el nombre del mes correspondiente.
+function mesActual(mes){
+    switch (mes){
+        case 1:
+            console.log('Enero')
+            break;
+        case 2:
+            console.log('Febrero')
+            break;
+        case 3:
+            console.log('Marzo')
+            break;
+        case 4:
+            console.log('Abril')
+            break;
+        case 5:
+            console.log('Mayo')
+            break;
+        case 6:
+            console.log('Junio')
+            break;
+        case 7:
+            console.log('Julio')
+            break;
+        case 8:
+            console.log('Agosto')
+            break;
+        case 9:
+            console.log('Septiembre')
+            break;
+        case 10:
+            console.log('Octubre')
+            break;
+        case 11:
+            console.log('Noviembre')
+            break;
+        case 12:
+            console.log('Diciembre')
+            break;
+        default:
+            console.log('No ingresaste un caracter valido, ingresa nuevamente')
+            break;
+    }
+}
+mesActual(11);
+
+// 2) Crea un programa que reciba un número del 1 al 7 y muestre el día de la semana correspondiente.
+function diaSemana(dia){
+    switch (dia){
+        case 1:
+            console.log('Lunes')
+            break;
+        case 2:
+            console.log('Martes')
+            break;
+        case 3:
+            console.log('Miercoles')
+            break;
+        case 4:
+            console.log('Jueves')
+            break;
+        case 5:
+            console.log('Viernes')
+            break;
+        case 6:
+            console.log('Sabado')
+            break;
+        case 7:
+            console.log('Domingo')
+            break;
+        default:
+            console.log('No ingresaste un caracter valido, ingresa nuevamente')
+            break;
+    }
+}
+diaSemana(3);
+
+// 3) Crea un programa que reciba un carácter y muestre si es una vocal o una consonante.
+function esVocal(letra){
+    if('aeiou'.includes(letra)){
+        console.log("Es una vocal");
+    }else if('0123456789'.includes(letra)){
+        console.log("Es numero");
+    }else{
+        console.log("Es consonante");
+    }
+}
+esVocal('i');
+
+/*4) Crea un programa que reciba un número del 1 al 5 y muestre el nombre del día de la semana 
+correspondiente a esa posición en un arreglo.*/
