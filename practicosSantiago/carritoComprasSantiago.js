3a5322f0d52467a0dbb45554e0fbc413610ccb3f
Santiago Soraire <eneasvallejo04@gmail.com>
2023-11-22 14:58:21 -0300

    Commit del carrito de compras

---
+++ practicosSantiago/carritoComprasSantiago.js
@@ -0,0 +1,70 @@
+//  Crear un carrito de compras.
+//  Crear una aplicación de carrito de compras donde se ingresen productos como elementos en un array.
+//  las tareas que debe hacer son las siguientes:
+//  Agregar producto al carrito
+//  Listar los productos del carrito
+//  Buscar un producto en el carrito
+//  Filtrar productos que coincidan con una palabra, parte de ella o una letra
+//  Eliminar producto del carrito
+
+console.log("===========\nBIENVENIDO A NUESTRA TIENDA\n===========")
+let carrito = []
+
+// Función para agregar productos al array (en este caso carrito)
+
+const agregarProductos = (array, producto) => {
+    producto = producto.toLowerCase()
+    array.push(producto)
+}
+
+// Función para ver los productos del array (en este caso carrito)
+
+const listaDeProductos = () => {
+    console.log("===========\nLISTA DE PRODUCTOS\n===========")
+    for (let i = 0; i < carrito.length; i++) {
+        console.log (carrito[i])
+    }
+}
+
+// Función para buscar productos y su posicion dentro del array (en este caso carrito)
+
+const busquedaDeProducto = (productoABuscar) => {
+    productoABuscar = productoABuscar.toLowerCase()
+    for (let i = 0; i < carrito.length; i++) {
+        if (productoABuscar === carrito [i]) {
+            return console.log("El producto " + productoABuscar + " fue encontrado en el Carrito, su posicion es la número " + i)
+        } else {
+            return console.log("Lo sentimos, no pudimos encontrar " + productoABuscar + " en el carrito ¿Probaste agregandolo?")
+        }
+    }
+}
+
+// Función para filtrar productos
+
+const filtroDeBusqueda = (palabra) => {
+    palabra = palabra.toLowerCase()
+    let productosCoincidentes = []
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].includes(palabra)) {
+            productosCoincidentes.push(carrito[i])
+        }
+    }
+    if (productosCoincidentes.length === 0) {
+        return console.log("La palabra que ingresaste no coincide con ningún producto del carrito")
+    } else {
+        return console.log(palabra + " coincide con los siguientes productos: " + productosCoincidentes)
+    }
+}
+
+// Función para borrar productos
+
+const eliminarProducto = (palabra) => {
+    palabra = palabra.toLowerCase()
+    indice = carrito.indexOf(palabra)
+    if (indice !== -1) {
+        let productoBorrado = carrito.splice(indice, 1)
+        console.log (palabra + " se ha eliminado correctamente del carrito!")
+    } else {
+        console.log (palabra + " no se ha podido eliminar porque no se encontraba en el carrito")
+    }
+}
