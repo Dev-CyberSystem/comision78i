b2c996c24754d813cd1677cb51af1af66acd99c4
Gadd88 <matiassaade@gmail.com>
2023-11-30 18:42:42 -0300

    tarea

--- jsMatias/4taTarea.js
+++ jsMatias/4taTarea.js
@@ -142,98 +142,6 @@ const esMayus = (letra) =>{
 }
 // esMayus('x')
 
-
-// 6) Crea un programa que reciba una nota del 0 al 10 y muestre la calificación correspondiente (suspenso, aprobado, notable, sobresaliente).
-const calificacion = (nro) =>{
-    switch(nro){
-const esMes = (num) =>{
-    switch (num) {
-        case 1:
-            console.log('Enero')
-            break;
-        case 2:
-            console.log('Febrero')
-            break;
-        case 3:
-            console.log('Marzo')
-            break;
-        case 4:
-            console.log('Abril')
-            break;
-        case 5:
-            console.log('Mayo')
-            break;
-        case 6:
-            console.log('Junio')
-            break;
-        case 7:
-            console.log('Julio')
-            break;
-        case 8:
-            console.log('Agosto')
-            break;
-        case 9:
-            console.log('Septiembre')
-            break;
-        case 10:
-            console.log('Octubre')
-            break;
-        case 11:
-            console.log('Noviembre')
-            break;
-        case 12:
-            console.log('Diciembre')
-            break;
-        default:
-            console.log('No ingresaste un caracter valido')
-            break;
-    }
-}
-// esMes(2)
-
-// 2) Crea un programa que reciba un número del 1 al 7 y muestre el día de la semana correspondiente.
-const esDia = (num) =>{
-    switch (num){
-        case 1:
-            console.log('Lunes')
-            break;
-        case 2:
-            console.log('Martes')
-            break;
-        case 3:
-            console.log('Miércoles')
-            break;
-        case 4:
-            console.log('Jueves')
-            break;
-        case 5:
-            console.log('Viernes')
-            break;
-        case 6:
-            console.log('Sábado')
-            break;
-        case 7:
-            console.log('Domingo')
-            break;
-        default:
-            console.log('No ingresaste un número válido')
-            break;
-    }
-}
-// esDia(3);
-
-// 3) Crea un programa que reciba un carácter y muestre si es una vocal o una consonante.
-const esVocal = (a) =>{
-    if('aeiou'.includes(a)){
-        console.log('es una vocal')
-    }else if('0123456789'.includes(a)){
-        console.log('es un número')
-    }else{
-        console.log('es una consonante')
-    }
-}
-// esVocal('a')
-
 // 4) Crea un programa que reciba un número del 1 al 5 y muestre el nombre del día de la semana correspondiente a esa posición en un arreglo.
 const esNumero = (num) => {
     const dias = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
@@ -261,19 +169,6 @@ const esNumero = (num) => {
 }
 // esNumero(1)
 
-// 5) Crea un programa que reciba una letra y muestre si es mayúscula o minúscula.
-const esMayus = (a) =>{
-    const mayus = 'abcdefghijklmnñopqrstuvwxyz'
-    const nros = '0123456789'
-    if(mayus.includes(a)){
-        console.log('es minuscula')
-    }else if(nros.includes(a)){
-        console.log('es un numero')
-    }else{
-        console.log('es MAYUSCULA')
-    }
-}
-// esMayus('g')
 
 // 6) Crea un programa que reciba una nota del 0 al 10 y muestre la calificación correspondiente (suspenso, aprobado, notable, sobresaliente).
 const calificacion = (num) =>{
@@ -347,20 +242,6 @@ const masGrande = () =>{
 }
 // masGrande()
 
-
-// Pedir al usuario que ingrese una palabra y mostrar en la consola si la palabra es un palíndromo o no.
-
-
-// Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola la suma de los números pares.
-// Pedir al usuario que ingrese una serie de números separados por coma y mostrar en la consola la cantidad de números negativos.
-            console.log('SObresaliente')
-            break
-        default:
-            console.log('numero invalido')
-            break
-    }
-}
-// calificacion(4)
 
 // Imprimir los números pares del 2 al 20 en la consola.
 const esPar = () =>{
@@ -456,4 +337,4 @@ const sumaPares = () =>{
 // Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original, pero sumándoles 1.
 // Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el último.
 // Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el primero y el último.
-// Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el número que se encuentre en la posición X.
+// Dado un array de números, escribir una función que retorne un nuevo array con todos los números del array original menos el número que se encuentre en la posición X
