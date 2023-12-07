ee6326abece10a8f2ca3a670d85865a72a7e355c
LucianoCV01 <Lucianocosentino01@gmail.com>
2023-12-06 23:35:48 -0300

    Funcion obtener usuario terminada, muestra la informacion de un objeto usuario

---
+++ javascript/primeraJs.js
@@ -0,0 +1,44 @@
+function sumarNumeros() {
+    let num1 = 10
+    let num2 = 20
+    console.log("Valor de Variable num2" + - num2) // Mostrame por consola el valor de num2
+    // esta es la suma de los numeros
+    let suma = num1 + num2
+    const nombre = "Diego"
+    console.log("Hola " + nombre) 
+    document.write("La suma es: " + suma)
+}
+sumarNumeros()
+console.log("Valor de Variable num2 afuera" + - num2)
+
+10 == 10 // true
+10 === 10 // true
+10 === "10" // false
+10 == "10" // true
+10 == "diez"  // false
+10 != "diez" // true
+10 != 10 // false
+10 !== "10" // true
+10 !== 10 // false
+10 > 10 // false
+10 >= 10 // true
+10 < 10 // false
+10 <= 10 // true
+// && // and
+// || // or
+
+// if(10 && 20) {
+//     console.log("Hola")
+// }
+// if(10 || 20) {
+//     console.log("Hola")
+// }
+
+// Crear Variables con let y const
+// Variables con tipos de datos: Number, String, Boolean
+// Usar la consola, Usar script en html.
+// Tienen que tener un index.html y un archivo .js
+// Nombres de los archivos primerPractico.js
+
+
+
