4fff3107ba89425f664eb5c30bdfbba08cb09060
Gadd88 <matiassaade@gmail.com>
2023-12-11 09:57:06 -0300

    tarea

--- jsMatias/7maTarea.js
+++ jsMatias/7maTarea.js
@@ -5,15 +5,27 @@ const obtenerUsuarios = async () => {
         let respuesta = await fetch("https://jsonplaceholder.typicode.com/users")
         let usuarios = await respuesta.json()
         console.log(usuarios[0])
-        console.log(typeof usuarios[0].address)
         usuarios.forEach(usuario => {
         const li = document.createElement('li')
         listaUsuario.appendChild(li)
-        for(let i = 1; i < Object.keys(usuario).length; i++){
-            let parr = document.createElement('p')
-            parr.textContent = `${Object.keys(usuario)[i]} : ${Object.values(usuario)[i]}`
-            li.appendChild(parr)
-        }
+        let info = document.createElement('p')
+        info.textContent = `
+            Name: ${usuario.name}
+            \nEmail: ${usuario.email}
+            \nTel: ${usuario.phone}
+            \nAddress: ${usuario.address.street} - ${usuario.address.city}
+        `
+        li.appendChild(info)
+        // for(let i = 1; i < Object.keys(usuario).length; i++){
+        //     let parr = document.createElement('p')
+        //     if(Object.keys(usuario)[i] == 'address'){
+        //         console.log(Object.values(Object.keys(usuario)[i] == 'address'))
+        //     }
+        //     parr.textContent = `
+        //         ${Object.keys(usuario)[i]} : ${Object.values(usuario)[i]}
+        //     `
+        //     li.appendChild(parr)
+        // }
         })
         // });
     }catch(err){
