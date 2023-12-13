69d2fc50ca9a9f9448b781a423be59d4fdb78359
Diego Olmi <diegolmi@hotmail.com>
2023-12-13 10:21:30 -0300

    Promesas, import && Export, BOM, LocalStorage

---
+++ javascript/import.js
@@ -0,0 +1,15 @@
+import { saludar, producto, variable } from "./export.js"
+import { carrito } from "./octava.js"
+
+console.log(carrito)
+
+ 
+
+console.log(carrito.forEach(producto => console.log(producto, "producto")), " Nombre de producto")
+
+
+console.log(saludar("Diego"))
+
+console.log(variable)
+
+console.log(producto, "producto")
