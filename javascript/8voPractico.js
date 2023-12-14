40166f33c34a1f1bc2e17fa22c255c6cd716a54c
lourdesreyna <reynalourdes91@gmail.com>
2023-12-14 11:46:45 -0300

    inicio de la tarea n° 8

---
+++ javascript/8voPractico.js
@@ -0,0 +1,20 @@
+//Tarea
+// ○ Nos piden realizar una agenda telefónica de contactos.
+// ○ Un contacto está definido por un nombre y un teléfono. Se considera que un
+// contacto es igual a otro cuando sus nombres son iguales.
+// ○ Una agenda de contactos está formada por un conjunto de contactos. Se
+// podrá crear de dos formas, indicando nosotros el tamaño o con un tamaño
+// por defecto (10).
+// ○ Las funciones serán:
+// ■ aniadirContacto(Contacto): Añade un contacto a la agenda, sino la
+// agenda no puede almacenar mas contactos indicar por pantalla.
+// ■ existeContacto(Conctacto): indica si el contacto pasado existe o no.
+// ■ listarContactos(): Lista toda la agenda
+// ■ buscarContacto(nombre): busca un contacto por su nombre y muestra
+// su teléfono.
+// ■ eliminarContacto(Contacto): elimina el contacto de la agenda, indica
+// si se ha eliminado o no por pantalla
+// ■ agendaLlena(): indica si la agenda está llena.
+// ■ huecosLibres(): indica cuántos contactos más podemos ingresar.
+// ○ Usar LocalStorage para guardar la info de la agenda y para consultar sus
+// datos
